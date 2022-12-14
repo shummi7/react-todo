7318bdb36dabb672eb293fca78c56d340306b04f
shummi7 <sharmila16moorthi@gmail.com>
2022-12-14 15:15:21 -0600

    2. with json server

--- src/App.js
+++ src/App.js
@@ -2,44 +2,64 @@ import "./App.css";
 import AddTodo from "./components/AddTodo";
 import ListTodo from "./components/ListTodo";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [storage, setStorage] = useState([
-    {
-      text: "Water Lilies",
-      checked: false,
-      id: 1,
-    },
-    {
-      text: "Clean Backyard",
-      checked: false,
-      id: 2,
-    },
-    {
-      text: "Steam Mop",
-      checked: false,
-      id: 3,
-    },
-  ]);
+  const [storage, setStorage] = useState([]);
+
+  useEffect(() => {
+    const getStorage = async () => {
+      const response = await fetch("http://localhost:5000/storage");
+      const result = await response.json();
+      setStorage(result);
+    };
+    getStorage();
+  }, [storage]);
 
   const addToStorage = (text) => {
     let newTodo = { checked: false };
     newTodo.text = text;
     newTodo.id = Math.floor(Math.random() * 100);
-    setStorage([...storage, newTodo]);
+
+    fetch("http://localhost:5000/storage", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(newTodo),
+    });
+    // setStorage([...storage, newTodo]);
   };
 
   const delFromStorage = (id) => {
-    setStorage(storage.filter((todo) => todo.id !== id));
+    fetch(`http://localhost:5000/storage/${id}`, {
+      method: "DELETE",
+    });
+    // setStorage(storage.filter((todo) => todo.id !== id));
   };
 
-  const checkboxChange = (id) => {
-    setStorage(
-      storage.map((todo) =>
-        todo.id === id ? { ...todo, checked: !todo.checked } : todo
-      )
-    );
+  const checkboxChange = async (id) => {
+    let fetchTodo = async () => {
+      let response = await fetch(`http://localhost:5000/storage/${id}`);
+      let data = await response.json();
+      return data;
+    };
+    let getTodo = await fetchTodo();
+    let updatedTodo = { ...getTodo, checked: !getTodo.checked };
+
+    fetch(`http://localhost:5000/storage/${id}`, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(updatedTodo),
+    });
+
+    // setStorage(
+    //   storage.map((todo) =>
+    //     todo.id === id ? { ...todo, checked: !todo.checked } : todo
+    //   )
+    // );
   };
 
   return (
